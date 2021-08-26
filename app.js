bc84c50ee151f34e1fd92df78ce10a3ae37f9dca
Eric Vincent Bermudez <eric.bermudez@appetiser.com.au>
2021-08-26 19:07:27 +0800

    Update app.js
    Add initial scraped data

--- app.js
+++ app.js
@@ -4,4 +4,23 @@ const puppeteer = require("puppeteer")
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
     await page.goto("https://www.onlinejobs.ph/jobseekers/jobsearch")
+
+    /* Run javascript inside the page */
+    const data = await page.evaluate(() => {
+        const list = []
+        const items = document.querySelectorAll(".results")
+
+        for (const item of items) {
+            list.push({
+                company: item.querySelector(".jobpost-cat-box a p").innerHTML,
+                rate: item.querySelector(".jobpost-cat-box a dd").innerHTML,
+                position: item.querySelector(".jobpost-cat-box a dt").innerHTML,
+            })
+        }
+
+        return list
+    })
+
+    console.log(data)
+    await browser.close()
 })()
