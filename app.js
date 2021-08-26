468c3e83eb946a976b5de847cff9a56b83ddbe6f
Eric Vincent Bermudez <eric.bermudez@appetiser.com.au>
2021-08-26 22:45:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
 const puppeteer = require("puppeteer")
+const mongo = require("mongodb").MongoClient
 
-;(async () => {
-    const browser = await puppeteer.launch({ headless: false })
-    const page = await browser.newPage()
-    await page.goto("https://www.onlinejobs.ph/jobseekers/jobsearch")
+const url = "mongodb://localhost:27017"
+let db, jobs
 
-    /* Run javascript inside the page */
-    const data = await page.evaluate(() => {
-        const list = []
-        const items = document.querySelectorAll(".jobpost-cat-box")
-
-        for (const item of items) {
-            list.push({
-                company: item.querySelector(".jobpost-cat-box a p").innerHTML,
-                rate: item.querySelector(".jobpost-cat-box a dd").innerHTML,
-                position: item.querySelector(".jobpost-cat-box a dt h4").innerHTML,
-                link: 'https://www.onlinejobs.ph' + item.querySelector(".jobpost-cat-box a")
-                    .getAttribute("href"),
-            })
+mongo.connect(
+    url,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err, client) => {
+        if (err) {
+            console.error(err)
+            return
         }
+        db = client.db("jobs")
+        jobs = db.collection("jobs")
 
-        return list
-    })
+        ;(async () => {
+            const browser = await puppeteer.launch({ headless: false })
+            const page = await browser.newPage()
+            await page.goto("https://www.onlinejobs.ph/jobseekers/jobsearch")
 
-    console.log(data)
-    await browser.close()
-})()
+            /* Run javascript inside the page */
+            const data = await page.evaluate(() => {
+                const list = []
+                const items = document.querySelectorAll(".jobpost-cat-box")
+
+                for (const item of items) {
+                    list.push({
+                        company: item.querySelector(".jobpost-cat-box a p").innerHTML,
+                        rate: item.querySelector(".jobpost-cat-box a dd").innerHTML,
+                        position: item.querySelector(".jobpost-cat-box a dt h4").innerHTML,
+                        link: 'https://www.onlinejobs.ph' + item.querySelector(".jobpost-cat-box a")
+                            .getAttribute("href"),
+                    })
+                }
+
+                return list
+            })
+
+            console.log(data)
+            jobs.deleteMany({})
+            jobs.insertMany(data)
+            await browser.close()
+        })()
+    }
+)
